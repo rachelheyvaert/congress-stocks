8e49557a3a2cbfe720df8aef6bc482d8efd7ba9b
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-03-08 06:32:35 -0500

    fixed index.js linking to HTML and created fetch function

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //global scope
 const fetchURL = "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
 const form = document.getElementById('stock-search-form')
-document.addEventListener(DOMContentLoaded, ()=> {
-
+const submitBtn = document.getElementsByName('submit')
+document.addEventListener('DOMContentLoaded', ()=> {
+handleSubmit()
 })
 
 //fetching data
-function handleSubmit
+function handleSubmit(){
+fetch(fetchURL)
+.then(response => response.json())
+.then(data => console.log(data))
+}
 
 //render data
