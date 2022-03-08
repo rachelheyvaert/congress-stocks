d7b1c355bccef96b16dcb819b5104f9670d3a388
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-03-08 06:48:20 -0500

    global scope and grabbing html elements

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-//global scope
+//grabbing html elements in global scope
 const fetchURL = "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
 const form = document.getElementById('stock-search-form')
 const submitBtn = document.getElementsByName('submit')
+const conContainer = document.getElementById('congress-container')
+const ul = document.getElementById('stock-list')
+let memberInfo; 
+
 document.addEventListener('DOMContentLoaded', ()=> {
-handleSubmit()
+getCongressData()
 })
 
 //fetching data
-function handleSubmit(){
-fetch(fetchURL)
+function getCongressData(){
+fetch(fetchURL) 
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => console.log(data))}
+//forEach Render
+
+function displayConMember() {
+//create elements
 }
 
-//render data
+//renderData
+
+//display Member
