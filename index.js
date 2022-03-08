3f711e14729b17ab5a1a99b0c7ef14abba2a679c
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-03-08 08:58:25 -0500

    innerHTML added for specfic members

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //grabbing html elements in global scope
 const fetchURL = "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
+const senateURL = "https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com/aggregate/all_transactions.json"
 const form = document.getElementById('stock-search-form')
 const submitBtn = document.getElementsByName('submit')
 const conContainer = document.getElementById('congress-container')
@@ -17,13 +18,38 @@ fetch(fetchURL)
 .then(data => console.log(data))}
 //forEach Render
 
-function displayConMember() {
-//create elements
-const repNameH2 = document.createElement('h2')
-const ownerP = document.createElement('p')
-const assetDescription = document.createElement('p')
-const amountP = document.createElement('p')
+function displayConMember(e) {
+    console.log(typeOf(e.target.id))
+    ul.innerHTML = ""
+    const member = member.find(m => m.id === parseInt(e.target.id))
+    console.log(member)
+    ul.innerHTML = `
+    <h1>${member.representative}</h1>
+    <h3>Asset Description: </h3>
+    <p>${member.asset_description}</p>
+    <h3>Stock Symbol/Ticker: </h3>
+    <p>${member.ticker}</p>
+    <h3>Amount: </h3>
+    <p>${member.amount}</p>
+    <h3>Transaction Date: </h3>
+    <p>${member.transaction_date}</p>
+    
+    
+    `
+// const repNameH2 = document.createElement('h2')
+// const ownerP = document.createElement('p')
+// const assetDescription = document.createElement('p')
+// const amountP = document.createElement('p')
+// ul.innerHTML = ''
+// repNameH2.textContent = person.representative
+// ownerP.textContent = person.owner
+// assetDescription.textContent = person.asset_description
+// amountP.textContent = person.amount
+// ul.append(repNameH2, assetDescription, amountP, ownerP)
 }
-//renderData
+
+
+
+
 
 //display Member
