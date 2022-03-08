48b2a818c149a60dc656ee768755de313ad68d84
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-03-08 06:22:40 -0500

    Global scope variables

---
+++ index.js
@@ -0,0 +1,11 @@
+//global scope
+const fetchURL = "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
+const form = document.getElementById('stock-search-form')
+document.addEventListener(DOMContentLoaded, ()=> {
+
+})
+
+//fetching data
+function handleSubmit
+
+//render data
